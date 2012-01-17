6de7024ed902f113012beca8fcf32c5fab418b08
Bram Swenson <bram@assistly.com>
2012-01-16 18:10:53 -0800

    CentOS support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,18 @@
 class memcached::params {
   case $::operatingsystem {
-    /(Ubuntu|Debian)/: {
-        $package_name = 'memcached'
-        $service_name = 'memcached'
-        $config_file = '/etc/memcached.conf'
+    ubuntu, debian: {
+      $package_name = 'memcached'
+      $service_name = 'memcached'
+      $config_file  = '/etc/memcached.conf'
+      $config_tmpl  = "${module_name}/memcached.conf.erb"
+      $default_user = 'nobody'
+    }
+    centos, redhat: {
+      $package_name = 'memcached'
+      $service_name = 'memcached'
+      $config_file = '/etc/sysconfig/memcached'
+      $config_tmpl  = "${module_name}/memcached_sysconfig.erb"
+      $default_user = 'memcached'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")

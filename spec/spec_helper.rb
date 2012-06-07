3e918bde1b8a74e0919b85f68882c72da4ba528e
François Charlier <francois.charlier@enovance.com>
2012-06-07 12:26:01 +0200

    rake spec runs without modification
    Add an empty site.pp file in spec/fixtures/manifests
    Update spec_helper.rb to find the manifest in fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,5 @@ end
 
 RSpec.configure do |c|
   c.module_path = File.join(File.dirname(__FILE__), '../../')
+  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
 end

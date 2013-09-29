36a7d4b5215685b5399c112d58c1027f07a4eb0e
Steffen Zieger <me@saz.sh>
2013-09-29 16:12:05 +0200

    use braces around variables

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class memcached::params {
       $service_hasstatus = false
       $dev_package_name  = 'libmemcached-dev'
       $config_file       = '/etc/memcached.conf'
-      $config_tmpl       = "$module_name/memcached.conf.erb"
+      $config_tmpl       = "${module_name}/memcached.conf.erb"
       $user              = 'nobody'
     }
     'RedHat': {
@@ -15,7 +15,7 @@ class memcached::params {
       $service_hasstatus = true
       $dev_package_name  = 'libmemcached-devel'
       $config_file       = '/etc/sysconfig/memcached'
-      $config_tmpl       = "$module_name/memcached_sysconfig.erb"
+      $config_tmpl       = "${module_name}/memcached_sysconfig.erb"
       $user              = 'memcached'
     }
     default: {

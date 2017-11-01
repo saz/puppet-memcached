50b6024cc43e4cd2f7475d49584d9d48fbad2a12
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2017-11-01 14:28:27 +0200

    Use meaningful heredoc for memcached_max_memory.rb
    It was generating a rubocop warning.

--- lib/puppet/parser/functions/memcached_max_memory.rb
+++ lib/puppet/parser/functions/memcached_max_memory.rb
@@ -1,7 +1,7 @@
 module Puppet::Parser::Functions
-  newfunction(:memcached_max_memory, :type => :rvalue, :doc => <<-EOS
+  newfunction(:memcached_max_memory, :type => :rvalue, :doc => <<-ENDHEREDOC
     Calculate max_memory size from fact 'memsize' and passed argument.
-    EOS
+    ENDHEREDOC
   ) do |arguments|
 
     if arguments.size != 1

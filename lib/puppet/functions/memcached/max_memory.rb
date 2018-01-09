2f8240c4efc31d6104ef0ca9e582750bcbf10c78
Steffen Zieger <me@saz.sh>
2018-01-09 22:35:31 +0100

    fix newly introduced rubocop issues

--- lib/puppet/functions/memcached/max_memory.rb
+++ lib/puppet/functions/memcached/max_memory.rb
@@ -9,28 +9,28 @@ Puppet::Functions.create_function(:'memcached::max_memory') do
     scope = closure_scope
     memsize = scope['facts']['memorysize']
 
-    if arg and !arg.to_s.end_with?('%')
+    if arg && !arg.to_s.end_with?('%')
       result_in_mb = arg.to_i
     else
       max_memory_percent = arg ? arg : '95%'
 
       # Taken from puppetlabs-stdlib to_bytes() function
-      value,prefix = */([0-9.e+-]*)\s*([^bB]?)/.match(memsize)[1,2]
+      value, prefix = *%r{([0-9.e+-]*)\s*([^bB]?)}.match(memsize)[1, 2]
 
       value = value.to_f
       case prefix
-        when '' then value = value
-        when 'k' then value *= (1<<10)
-        when 'M' then value *= (1<<20)
-        when 'G' then value *= (1<<30)
-        when 'T' then value *= (1<<40)
-        when 'E' then value *= (1<<50)
-        else raise Puppet::ParseError, "memcached_max_memory(): Unknown prefix #{prefix}"
+      when '' then value = value
+      when 'k' then value *= (1 << 10)
+      when 'M' then value *= (1 << 20)
+      when 'G' then value *= (1 << 30)
+      when 'T' then value *= (1 << 40)
+      when 'E' then value *= (1 << 50)
+      else raise Puppet::ParseError, "memcached_max_memory(): Unknown prefix #{prefix}"
       end
       value = value.to_i
-      result_in_mb = ( (value / (1 << 20) ) * (max_memory_percent.to_f / 100.0) ).floor
+      result_in_mb = ((value / (1 << 20)) * (max_memory_percent.to_f / 100.0)).floor
     end
 
-    return result_in_mb
+    result_in_mb
   end
 end

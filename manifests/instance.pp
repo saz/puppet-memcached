19db2f39cd52e1d6cfc316d7745fe2ec302b8d81
Steffen Zieger <me@saz.sh>
2021-06-11 10:04:55 +0200

    no need to force systemd reload anymore
    Puppet 6 and 7 are handling reloading of systemd properly

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -52,7 +52,6 @@ define memcached::instance (
       unit          => $service_name,
       source        => $override_source,
       content       => $override_content,
-      daemon_reload => 'eager',
       notify        => Service[$service_name],
     }
   }

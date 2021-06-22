e15547553779df3d959516ad38ed3d1fea6394b9
Steffen Zieger <steffen.zieger@regiohelden.de>
2021-06-22 12:11:22 +0200

    Update from saz modulesync_config

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,4 +1,6 @@
-# This file is completely based on voxpupuli/modulesync_config
+# This file is completely managed via modulesync
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker
+
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
